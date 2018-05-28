b2364c9d0fbf9e7ea0c2d50051f5274a6bd59e63
UchuNoKen <vanguardbosses@gmail.com>
2018-05-28 16:46:00 -0400

    Babel watch test

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,13 +1,15 @@
-"use strict";
+'use strict';
 
 console.log('App.js is running');
 
 // JSX - JavaScript XML
 
 // will not render without babel
-//var template = <p>This is JSX from app.js</p>;  
-var template2 = React.createElement("h1", { id: "app" }, "Create new element without transpiling");
-
+var template = React.createElement(
+  'p',
+  null,
+  'Is this being watched?'
+);
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template2, appRoot);
+ReactDOM.render(template, appRoot);
