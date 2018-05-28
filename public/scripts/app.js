82bde010cd5a1d57426b25b784fe1e0a8e525b53
UchuNoKen <vanguardbosses@gmail.com>
2018-05-28 16:58:49 -0400

    Root element wrapper

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,10 +6,20 @@ console.log('App.js is running');
 
 // will not render without babel
 var template = React.createElement(
-  'h1',
+  'div',
   null,
-  'Indecision App'
+  React.createElement(
+    'h1',
+    null,
+    'Indecision App'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'This is some info'
+  )
 );
+
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
--- src/app.js
+++ src/app.js
@@ -3,7 +3,12 @@ console.log('App.js is running');
 // JSX - JavaScript XML
 
 // will not render without babel
-var template = <h1>Indecision App</h1>;  
+var template = 
+<div>
+<h1>Indecision App</h1>
+<p>This is some info</p>
+</div>;
+ 
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
