f0b5c0379e6f2d6a77472a0b7da967f2d09e3016
UchuNoKen <vanguardbosses@gmail.com>
2018-05-28 14:19:49 -0400

    Add div to render. Created template to test React.createElement. Show use of babel to transpile down.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,2 +1,15 @@
 console.log('App.js is running');
 
+// JSX - JavaScript XML
+
+// will not render without babel
+//var template = <p>This is JSX from app.js</p>;  
+var template2 = React.createElement(
+    "h1",
+    {id: "app"},
+    "Create new element without transpiling"
+);
+
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template2, appRoot);
