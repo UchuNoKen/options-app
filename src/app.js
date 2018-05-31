95c5b706481530cff68252ef93b39b3ef2cc6e5a
UchuNoKen <vanguardbosses@gmail.com>
2018-05-30 20:25:09 -0400

    Add remove all button and tie to event handler

--- src/app.js
+++ src/app.js
@@ -3,26 +3,52 @@ console.log('App.js is running');
 const app = {
   title: 'Indecision App',
   subtitle: 'Your life, in the hands of technology',
-  options: ['One', 'Two']
+  options: []
 };
 
 // JSX - JavaScript XML
-const template = (
-  <div>
-    <h1>{app.title}</h1>
-    {app.subtitle && <p>{app.subtitle}</p>}
-    <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
-    <ol>
-      <li>Item one</li>
-      <li>Item two</li>
-    </ol>
-    <form>
-      <input type="text" name="option" />
-      <button>Add Option</button>
-    </form>
-  </div>
-); 
+
+const onFormSubmit = (e) => {
+  e.preventDefault();
+
+  const option = e.target.elements.option.value;
+
+    if(option){
+      app.options.push(option);
+      e.target.elements.option.value = '';
+      // console.log('Form submitted');
+      render();
+    }
+};
+
+const onRemoveAll = () => {
+  // console.log(app.options);
+  app.options = [];
+  render();
+};
 
 const appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+const render = () => {
+  const template = (
+    <div>
+      <h1>{app.title}</h1>
+      {app.subtitle && <p>{app.subtitle}</p>}
+      <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+      <p>{app.options.length}</p>
+      <button onClick={onRemoveAll}>Remove All</button>
+      <ol>
+        <li>Item one</li>
+        <li>Item two</li>
+      </ol>
+      <form onSubmit={onFormSubmit}>
+        <input type="text" name="option" />
+        <button>Add Option</button>
+      </form>
+    </div>
+  );
+  
+  ReactDOM.render(template, appRoot);
+};
+
+render();
