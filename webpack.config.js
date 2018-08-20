1247935771fa8f46edd8911d82e9621c95f5bbc9
UchuNoKen <vanguardbosses@gmail.com>
2018-08-20 16:19:21 -0400

    switched from npm to webpack

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+// entry -> output bundle file
+
+const path = require('path');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js'
+  }
+};
