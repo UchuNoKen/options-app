a9dc6ae6c98a87518b1bb7b19a3e56b5dd226ef3
UchuNoKen <vanguardbosses@gmail.com>
2018-08-25 17:35:14 -0400

    Final Indecision App Commit

--- src/app.js
+++ src/app.js
@@ -1,1 +1,8 @@
-console.log('App is running!');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import IndecisionApp from './components/IndecisionApp';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+
